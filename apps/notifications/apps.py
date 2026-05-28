a46dae14ccf592b4c71a9b0cc1cbf917e2d33db5
IsabellaAriasCue <iarias579@cue.edu.co>
2026-05-27 23:29:51 -0500

    envio de emails

--- apps/notifications/apps.py
+++ apps/notifications/apps.py
@@ -6,3 +6,7 @@ class NotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.notifications'
     verbose_name = 'Notificaciones'
+
+    def ready(self):
+        # Registra las signals al iniciar la app
+        import apps.notifications.signals  # noqa: F401
